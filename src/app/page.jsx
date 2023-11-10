2e1bb90c2454cbc590c407bc2bd83090f2f0334d
Renan <renan23eduardo@gmail.com>
2023-11-09 22:35:50 -0300

    Create base project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <Link href='/'>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque esse saepe non sapiente adipisci ipsum cum temporibus consequuntur facilis vero sint libero qui culpa, quisquam, exercitationem laborum ex ab. Saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, soluta adipisci molestiae libero reiciendis illum dolorem neque exercitationem quaerat iusto quia, minus velit deserunt error repellendus enim, eligendi laborum rem?</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quidem ab corrupti consequuntur totam, eaque soluta enim necessitatibus rerum quos possimus voluptatum error saepe voluptate repellat deleniti ratione assumenda voluptates.
+    </Link>;
 }
